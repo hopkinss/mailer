9209a08e19340e7449b409005b78a804a55480aa
Shawn Hopkins <shopkins@corp.seagen.com>
2018-06-24 11:53:13 -0700

    added visualization and subsetting

--- main.py
+++ main.py
@@ -3,43 +3,95 @@ import base64
 import requests
 
 from flask import Flask, render_template, request, redirect, url_for, session
-from model import Donation,Donor,get_total
+from model import Donation,Donor,get_total,get_donors,get_donor_list,get_donations_list
+from flask import make_response
+from functools import wraps, update_wrapper
+from datetime import datetime
 
 
 
 app = Flask(__name__)
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return update_wrapper(no_cache, view)
 
 
 @app.route('/')
 def home():
+    """
+    Default path
+    :return: link to all donations
+    """
     return redirect(url_for('all'))
+
+
 
 @app.route('/donations/')
 def all():
-    donations = Donation.select()
+    """
+    List of all donors
+    :return: All donations
+    """
+    donations=get_donors()
     return render_template('donations.jinja2', donations=donations)
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
+    """
+    Create a new donor if values are appropriate
+    :return:
+    """
     if request.method == 'POST':
+        name = request.form['name']
+        try:
+            val=float(request.form['donation'])
+            if val<10:
+                return render_template('create.jinja2',name=name)
+            else:
+                donor=Donor(name=request.form['name'])
+                donor.save()
+                Donation(donor=donor, value= float(request.form['donation'])).save()
+                return redirect(url_for('all'))
+        except ValueError:
+            return render_template('create.jinja2', name=name)
 
-        if request.form['name']=='':
-            return redirect(url_for('all'))
-
-        donor=Donor(name=request.form['name'])
-        donor.save()
-        Donation(donor=donor, value= float(request.form['donation'])).save()
-        return redirect(url_for('all'))
     else:
-        return render_template('create.jinja2')
+        return render_template('create.jinja2',name="")
 
 @app.route('/bar/')
+@nocache
 def bar():
     data = get_total()
     labels=list([i.name for i in data])
     values=list([i.total for i in data])
     max_val=max(values)
     return render_template('bar.jinja2', values=values, labels=labels,max_val=max_val)
+
+@app.route('/donors', methods=['GET', 'POST'])
+@nocache
+def donors():
+    if request.method == 'POST':
+        val = request.form['target']
+        donations = get_donations_list(val)
+        selected_donor=val
+        donor_list=get_donor_list()
+        return render_template('donors.jinja2',donor_list=donor_list, donations=donations,selected_donor=selected_donor)
+    else:
+
+        donor_list=get_donor_list()
+        donations=get_donations_list(donor_list[0])
+        selected_donor=donor_list[0]
+        return render_template('donors.jinja2', donor_list=donor_list,donations=donations,selected_donor=selected_donor)
+
+
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -24,3 +24,46 @@ def get_total():
           .group_by(Donor.name))
     return qry
 
+class DonorView():
+    def __init__(self,name):
+        self.name=name
+        self.donations=[]
+
+    def append_donation(self,donations):
+        self.donations="<br />".join(donations)
+
+
+def get_donors():
+    dlist=[]
+    donors=Donor.select()
+
+    for d in donors:
+        donor=DonorView(d.name)
+        vals=[]
+        for v in d.donations:
+            vals.append("$"+ str(v.value))
+
+        donor.append_donation(vals)
+        dlist.append(donor)
+    return dlist
+
+def get_donor_list():
+    dlist=[]
+    for v in Donor.select():
+        dlist.append(v.name)
+
+    return dlist
+
+def get_donations_list(name):
+    dlist = []
+    donors = Donor.select()
+
+    for d in donors.where(Donor.name==name):
+        dlist.append(name)
+        vals = []
+        for v in d.donations:
+            vals.append("$" + str(v.value))
+
+
+        dlist.append(vals)
+    return dlist
